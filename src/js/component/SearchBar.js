8d1340b80974319e39e62a47fff8d60132898684
Valentina Cuello <delmar_vale08@hotmail.com>
2021-06-01 17:18:25 +0000

    NavBar pronta

---
+++ src/js/component/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { List, PersonCircle, Search, Basket2Fill } from "react-bootstrap-icons";
+import "../../styles/navbar.scss";
+
+export const SearchBar = () => {
+	return (
+		<>
+			<div className="search-bar">
+				<Search className="icon" />
+				<input name="name" placeholder="Buscar..." className="search-input" />
+			</div>
+		</>
+	);
+};
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { List, PersonCircle, Search, Basket2Fill } from "react-bootstrap-icons";
+import Logo from "../../img/landa_logo.png";
+import { SearchBar } from "./SearchBar";
+import "../../styles/navbar.scss";
 
 export const Navbar = () => {
+	const [clicked, setClick] = useState(false);
+
+	const menuClick = () => {
+		setClick(!clicked);
+	};
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<>
+			<nav className="navbar-container">
+				<div className="icon-mobile" onClick={menuClick}>
+					<Search className="icon" />
+				</div>
+
+				<div className="logo">
+					<Link to="/">
+						<img src={Logo} />
+					</Link>
+				</div>
+				<div className="desktop-search">
+					<SearchBar />
+				</div>
+				<div className="icon-container">
+					<PersonCircle className="icon" />
+					<Basket2Fill className="icon" />
+				</div>
+			</nav>
+			<div className={"mobile-search" + (clicked == true ? " on" : "")}>
+				<SearchBar />
 			</div>
-		</nav>
+		</>
 	);
 };
